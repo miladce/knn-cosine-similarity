e60fa35d98b6ba6ac224d1c2c69832af5412f9ca
Milad Ghafouri <ininformatics@gmail.com>
2019-10-26 11:29:21 +0330

    add bucket_num

--- extended_similarities.py
+++ extended_similarities.py
@@ -6,12 +6,12 @@ class DistributedCosineKnn:
 	def __init__(self, k=3):
 		self.k=k
 
-	def fit(self, input_data, n_epoch=1):
+	def fit(self, input_data, n_bucket=1):
 	    idxs=[]
 	    dists=[]
-	    epochs = np.array_split(input_data,n_epoch)
-	    for b in range(n_epoch):
-	    	cosim = cosine_similarity(epochs[b], input_data)
+	    buckets = np.array_split(input_data,n_bucket)
+	    for b in range(n_bucket):
+	    	cosim = cosine_similarity(buckets[b], input_data)
 	    	idx0=[(heapq.nlargest((self.k+1), range(len(i)), i.take)) for i in cosim]
 	    	idxs.extend(idx0)
 	    	dists.extend([cosim[i][idx0[i]] for i in range(len(cosim))])
